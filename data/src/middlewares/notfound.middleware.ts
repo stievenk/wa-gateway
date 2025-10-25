eec3422c2713d8a3ef5f60ed596701bdd6e516b9
stievenk <stieven.kalengkian@gmail.com>
2025-10-26 07:40:28 +0800

    del data

--- data/src/middlewares/notfound.middleware.ts
+++
@@ -1,8 +0,0 @@
-import { NotFoundHandler } from "hono";
-import { HTTPException } from "hono/http-exception";
-
-export const notFoundMiddleware: NotFoundHandler = (c) => {
-  throw new HTTPException(404, {
-    message: "Route not found",
-  });
-};
